b0668ef4465b2266e31fbb18736a4813c6b85f0e
raghu12 <raghu.mj12@gmail.com>
2018-07-06 18:49:54 +0530

    Add files via upload

---
+++ LocateCenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocateCenterViewController.h
+//  GOK
+//
+//  Created by admin on 05/07/18.
+//  Copyright © 2018 MAC012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocateCenterViewController : UIViewController
+
+@end
